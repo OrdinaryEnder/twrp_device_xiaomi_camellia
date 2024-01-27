ec3c44bac83af5f482c87e0e389417fd6f4aa065
rio004 <riyon336@gmail.com>
2024-01-27 21:40:39 +0000

    camellia: Add necessary TWRP flags
    Change-Id: If7603ad0146dfc9208daed11d09f964fbaffc32e
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfigTWRP.mk
+++ BoardConfigTWRP.mk
@@ -10,5 +10,14 @@ TW_EXCLUDE_APEX := true
 # Encryption
 TW_INCLUDE_CRYPTO := true
 
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+VENDOR_SECURITY_PATCH := 2099-12-31
+
+# UI
+TW_EXTRA_LANGUAGES := true
+TW_THEME := portrait_hdpi
+
 # USB
 TW_EXCLUDE_DEFAULT_USB_INIT := true
